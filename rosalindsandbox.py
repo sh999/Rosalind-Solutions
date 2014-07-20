18d455d3c59d52fb5e9439e01f680538a8f0e202
sh999 <satrio.h@gmail.com>
2014-07-20 19:56:50 -0400

    fibd solved

--- rosalindsandbox.py
+++ rosalindsandbox.py
@@ -6,33 +6,44 @@ count # of adults and create offspring
 '''
 import copy 
 
-ageLimit = 9
-a = [x for x in range(ageLimit)]
+
+
+generations = 80
+oldestAge = 16
+
+
+a = [x for x in range(oldestAge+2)]
 b = []
 for i in a:
 	b.append([i, 0])
 b[0] = [0, 1]
-oldestAge = 3
-#b = [[0, 1],[1, 2],[2,2],[3,0],[4,0],[5,0]] #before breed list
+
 c = copy.deepcopy(b) #holds after breed list
 adults = 0
 total = 0
 
-for gen in range(1, 5):
+for gen in range(1, generations):
+	adults = 0
 	print "\ngeneration = ", gen
 	print "Original b      = ", b
 
 	# Shift elements to the right
 	for i in range(oldestAge, -1, -1):
 		c[i+1][1] = c[i][1]
-	print "(c) After shift = ", c
+	c[0][1] = 0  # Age 0 bunny should be none (artifact of shift leaves nonzero value)
+	print "(c) After aging = ", c
 
 	# Breed 
+	c[oldestAge][1] = 0
+
 	for i in range(1, oldestAge+1):
 		adults = adults + b[i][1]
+	print("adults = ", adults)
+	# Kill
 	c[0][1] = adults
+	
 	b = copy.deepcopy(c)
-	print "b after process = ", b
+	print "b end of gen    = ", b
 	total = 0
 	for i in range(0, len(b)):
 		#print b[i][1]
