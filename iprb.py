25ac734e66bf3183486c05bc33ce326610089cdf
sh999 <satrio.h@gmail.com>
2014-07-23 21:06:27 -0400

    Solved Mendel's First Law

--- iprb.py
+++ iprb.py
@@ -1,19 +1,22 @@
-class individual:
-	def __init__(self, a1, a2):
-		self.a1 = a1
-		self.a2 = a2
-	def getAlleles(self):
-		return [a1, a2]
-	def mate(self, another_indiv):
-		children = []
-		for i in a:
-			for j in b:
-				children.append([i+j])
+#!usr/local/bin/python2.7-5
+import copy
+def dictFunc(dictio, mod):
+	x = dict(dictio)
+	total = sum(a.values())
+	prob1 = x[mod[0]]/total
+	x[mod[0]] -= 1
+	total -= 1
+	prob2 = x[mod[1]]/total
+	totalProb = prob1*prob2
+	print totalProb
+	return totalProb
 
-a = individual("A", "a")
-b = individual("A", "A")
-a.mate(b)
-print
-
-
+a = {'dom':20.0, 'het':23.0, 'rec':28.0}
+b = ('dom', 'dom')
+c = ('dom', 'het')
+d = ('dom', 'rec')
+e = ('het', 'het')
+f = ('het', 'rec')
+total = dictFunc(a, b) + dictFunc(a, c)*2 + dictFunc(a, d)*2 + dictFunc(a, e)*0.75 + dictFunc(a, f)*0.5*2
+print "total = ", total
 
--- rosalindsandbox.py
+++ rosalindsandbox.py
@@ -1,57 +1,18 @@
 #!/usr/local/bin/python2.7-32
 '''
-study of fibonacci mortal
-to do:
-count # of adults and create offspring
 '''
-import copy 
+
+a = 3
+b = 3
+c = 5
+a = c
+print a
+print b
+print c
 
 
 
-generations = 80
-oldestAge = 16
 
-
-a = [x for x in range(oldestAge+2)]
-b = []
-for i in a:
-	b.append([i, 0])
-b[0] = [0, 1]
-
-c = copy.deepcopy(b) #holds after breed list
-adults = 0
-total = 0
-
-for gen in range(1, generations):
-	adults = 0
-	print "\ngeneration = ", gen
-	print "Original b      = ", b
-
-	# Shift elements to the right
-	for i in range(oldestAge, -1, -1):
-		c[i+1][1] = c[i][1]
-	c[0][1] = 0  # Age 0 bunny should be none (artifact of shift leaves nonzero value)
-	print "(c) After aging = ", c
-
-	# Breed 
-	c[oldestAge][1] = 0
-
-	for i in range(1, oldestAge+1):
-		adults = adults + b[i][1]
-	print("adults = ", adults)
-	# Kill
-	c[0][1] = adults
-	
-	b = copy.deepcopy(c)
-	print "b end of gen    = ", b
-	total = 0
-	for i in range(0, len(b)):
-		#print b[i][1]
-		total = total + b[i][1]
-	print "total = ", total
-
-
-print "\nc After breed = ", c
 
 
 	
