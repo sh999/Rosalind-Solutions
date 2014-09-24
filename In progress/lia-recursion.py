121eed29d9d37146e4b33db15ba4d0f7b55ca76c
sh999 <satrio.h@gmail.com>
2014-09-24 07:46:38 -0400

    Added lia-recursion.py

---
+++ In progress/lia-recursion.py
@@ -0,0 +1,31 @@
+'''
+Input: a list containing one number
+Algorithm will generate two makeChildren
+On the left = double of parent
+Right = Triple of parent
+A list will hold 
+'''
+class Master:
+	
+	def __init__(self, a, limit):
+		self.topElement = a
+		self.allElements = [self.topElement]
+		self.limit = limit
+
+	def makeChildren(self, parent, counter):
+		if counter < self.limit:
+			print "Parent = ",parent
+			leftChild = 2 * parent
+			rightChild = 3 * parent
+			print "Left child = ",leftChild
+			print "Right child = ",rightChild
+			
+			self.allElements.extend([leftChild])
+			self.allElements.extend([rightChild])
+			self.makeChildren(leftChild, counter+1)
+			self.makeChildren(rightChild, counter+1)
+
+a = Master(1, 3)
+a.makeChildren(1, 0)
+print a.allElements
+
